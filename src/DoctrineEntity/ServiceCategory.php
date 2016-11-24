5cfbadd5a5ac2cc481f7cca29de5d3433266c3e9
yangshinan <yangsn@figo.cn>
2016-11-24 13:54:39 +0800

    改进服务类别doctrine实体，增加seters和getter

--- src/DoctrineEntity/ServiceCategory.php
+++ src/DoctrineEntity/ServiceCategory.php
@@ -49,4 +49,31 @@ class ServiceCategory
      * @JoinColumn(name="service_category_id", referencedColumnName="id")
      */
     protected $parent;
+
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setParent($parent)
+    {
+        $this->parent = $parent;
+    }
 }
