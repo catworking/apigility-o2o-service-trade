be86b07428caa8b92d7fef3f7425fb344cbfe990
yangshinan <yangsn@figo.cn>
2016-11-30 16:38:59 +0800

    添加认证配置

--- Module.php
+++ Module.php
@@ -10,10 +10,12 @@ class Module implements ApigilityProviderInterface
     {
         $doctrine_config = new Config(include __DIR__ . '/config/doctrine.config.php');
         $service_config = new Config(include __DIR__ . '/config/service.config.php');
+        $manual_config = new Config(include __DIR__ . '/config/manual.config.php');
 
         $module_config = new Config(include __DIR__ . '/config/module.config.php');
         $module_config->merge($doctrine_config);
         $module_config->merge($service_config);
+        $module_config->merge($manual_config);
 
         return $module_config;
     }
